ebab2423c63482040fc288dade5358e9e3b6aba3
Angel Miguel abg <milicangelkuyoc@gmail.com>
2024-09-21 19:54:04 -0500

    first commit

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>Historial</h3>
+        <ul id="list" class="list">
+            <li v-for="transaction in transactions"
+            v-bind:key="transaction.id">
+            {{ transaction.text }}
+            <span>-${{ transaction.amount }}</span>
+            <button class="deleta-btn">X</button>
+            </li>
+            <button class="delete-btn"></button>
+           <!--- <li class="minus">
+                Efectivo <span>-$400</span>
+                <button class="delete-btn"></button>
+            </li>
+            <li class="plus">
+                Pago <span>$8000</span>
+                <button class="delete-btn"></button>
+            </li> --->
+        </ul>
+
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true,
+    }
+});
+ </script>
